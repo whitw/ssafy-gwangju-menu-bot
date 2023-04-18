a023c93d23dd7580d99ea8cfee22c26ee8c26ab7
whitw <shkim7876@naver.com>
2023-04-18 16:19:08 +0900

    gwangju-menubot.py

--- gwangju-menubot.py
+++ gwangju-menubot.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from datetime import date
-import schedule
 
 def updateMenu():
     todayMenu = requests.request('GET','https://front.cjfreshmeal.co.kr/meal/v1/today-all-meal?storeIdx=6442')
@@ -37,43 +36,11 @@ def post(menu, url_hook):
                              headers=headers,
                              data=json.dumps({"text":menu}))
     
-def dailyUpdateAndPost(url_hook):
-    post(getRenderString(updateMenu()), url_hook)
-
 def dailyMenuUpdate():
-    url_hook_file = '/home/whitw7876/url_hook.txt'
+    url_hook_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'url_hook.txt')
     with open(url_hook_file, 'rt') as f:
         url_hook = f.read()
-    dailyUpdateAndPost(url_hook)
+    post(getRenderString(updateMenu(), url_hook)
 
 if __name__ == '__main__':
     dailyMenuUpdate()
-    
-'''
-# 가동 방법
-1. 적당한 24시간 가동 가능한 파이썬 환경이 필요합니다.
-저는 GCP 무료 설정 찾아서 그거대로 설정했어요.
-AWS나 Heroku같은 것도 있다니까 한번 알아보셔도 괜찮을듯
-
-2. 매터모스트 왼쪽 위
-ㅁㅁㅁ
-ㅁㅁㅁ
-ㅁㅁㅁ
-아이콘 > 통합 > 전체 Incoming Webhook > Incoming Webhook 추가해주세요.
-채널은 봇이 있는 채널 선택하면 됩니다.
-
-3. URL을 dailyMenuUpdate함수 url_hook에 붙여넣기 해주세요.
-
-4. pip로 requests, bs4, json, schedule중 설치가 되지 않은 라이브러리가 있다면 설치해주세요.
-
-5. 프로그램을 실행시킵니다.
-
-# 동작 원리
-https://front.cjfreshmeal.co.kr/meal/v1/today-all-meal?storeIdx=6442
-에서 json을 긁어와서 메터모스트 메시지로 바꾸어 보내는 방식입니다.
-
-# 제약
-일단 dailyMenuUpdate() 몇 번 실행시켜봐서 문제 없는건 확인 했는데
-며칠 더 돌려봐야 진짜 잘 돌아가는지 알 수 있어요ㅎㅎ;
-안되면 왜 안되는지는 저도 찾아봐야됩니다.
-'''
