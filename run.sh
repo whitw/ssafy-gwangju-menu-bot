cec9afe23669afc62df036eef30dc7584f348ddb
whitw <shkim7876@naver.com>
2023-04-19 09:29:10 +0900

    remove old images on building new one

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt install docker.io
+sudo docker rm -f $(sudo docker ps --filter ancestor=gwangju-menubot -q)
+sudo docker rmi gwangju-menubot
 sudo docker build -t gwangju-menubot .
 sudo docker run -d gwangju-menubot
